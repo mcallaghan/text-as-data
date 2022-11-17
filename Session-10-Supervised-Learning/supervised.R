63a3cd31330a7675d9e635967de64970fd4be547
Max Callaghan <max.w.callaghan@gmail.com>
2022-11-17 08:11:34 +0100

    Session 10 slides

--- Session-10-Supervised-Learning/supervised.R
+++ Session-10-Supervised-Learning/supervised.R
@@ -38,37 +38,124 @@ library(readr)
 library(tidyr)
 library(dplyr)
 df <- read_csv("../datasets/uk_manifestos.csv")
-df$climate <- 0
-df$climate[df$cmp_code==501] <- 1
-df$climate <- factor(df$climate)
+df<- df[sample(nrow(df)),]
+df$env <- 0
+df$env[df$cmp_code==501] <- 1
+df$env <- factor(df$env) 
+
+df <- df %>% select(-cmp_code) 
 
 
 library(textrecipes)
 library(tidymodels)
-library(caret)
 library(parsnip)
 library(rsample)
 library(yardstick)
 
-rec <-recipe(climate ~ text, data = df)
+df_split <- initial_split(df, prop=0.8)
+train_data <- training(df_split)
+test_data <- testing(df_split)
+
+rec <-recipe(env ~ text, data = train_data)
 
 rec <- rec %>%
   step_tokenize(text) %>%
   step_tokenfilter(text, max_tokens = 1e3) %>%
   step_tfidf(text)
 
-wf <- workflow() %>%
-  add_recipe(rec)
-
 model <- svm_linear(mode="classification")
 
-df_split <- initial_split(df)
-train_data <- training(df_split)
-test_data <- testing(df_split)
+wf <- workflow() %>%
+  add_recipe(rec) %>%
+  add_model(model)
 
-model_fit <- wf %>%
-  add_model(model) %>% 
+model_fit <- wf %>% 
   fit(train_data)
 
-predict(model_fit, test_data)
+test_data$prediction <- predict(model_fit, test_data)$.pred_1
 
+tp <- sum((test_data$env==1)&(test_data$prediction==1))
+fn <- sum((test_data$env==1)&(test_data$prediction==0))
+fp <- sum((test_data$env==0)&(test_data$prediction==1))
+tn <- sum((test_data$env==0)&(test_data$prediction==0))
+recall <- tp / (tp+fn)
+precision <- tp / (tp+fp)
+
+scorer <- metric_set(
+  yardstick::accuracy, 
+  yardstick::precision, 
+  yardstick::recall,
+  yardstick::f_meas
+)
+
+
+scorer(test_data, truth=env, estimate=prediction, event_level="second")
+
+
+model <- svm_poly(cost=tune()) %>%
+  set_engine("kernlab") %>%
+  set_mode("classification")
+
+wf <- workflow() %>%
+  add_recipe(rec) %>%
+  add_model(model)
+
+folds <- vfold_cv(train_data, v = 2)
+svm_res <- tune_grid(
+  wf, resamples = folds, grid = 2,
+  metrics = metric_set(f_meas),
+  control = control_grid(event_level="second")
+  )
+collect_metrics(svm_res)
+
+best_model <- svm_res %>% select_best()
+
+final_workflow <- wf %>% 
+  finalize_workflow(best_model)
+
+
+
+test_data$opt_prediction <- predict(model_fit, test_data)$.pred_class
+scorer(test_data, truth=env, estimate=opt_prediction)
+
+
+n_splits <- 3
+
+
+
+k <- 1
+k_split <- folds$splits[[k]]
+k_train <- training(k_split)
+k_test <- testing(k_split)
+k_model <- final_workflow %>% fit(k_train)
+k_test$pred <- predict(k_model, k_test, type="prob")$.pred_1
+t <- 0.5
+est <- factor(ifelse(k_test$pred>=t,1,0))
+
+
+
+f1 <- f_meas_vec(k_test$env, est, event_level = "second")
+
+
+
+res <- data.frame(k=numeric(),t=numeric(),f1=numeric())
+n_splits=3
+folds <- vfold_cv(train_data, v = n_splits)
+for (k in 1:n_splits) {
+  k_split <- folds$splits[[k]]
+  k_train <- training(k_split)
+  k_test <- testing(k_split)
+  k_model <- final_workflow %>% fit(k_train)
+  k_test$pred <- predict(k_model, k_test, type="prob")$.pred_1
+  for (t in seq(0.1, 0.9, length.out=50)) {
+    est <- factor(ifelse(k_test$pred>=t,1,0))
+    f1 <- f_meas_vec(k_test$env, est, event_level = "second")
+    res <- add_row(res,k=k,t=t,f1=f1)
+  }
+}
+
+res <- res %>% 
+  group_by(t) %>% summarise(f1 = mean(f1)) %>%
+  arrange(desc(f1))
+
+optimal_t <- res$t[1]
