f499e16ef428b2d832b79ceb770a74f2b09200b5
Max Callaghan <max.w.callaghan@gmail.com>
2023-11-19 22:46:03 +0100

    supervised updates

--- Session-10-Supervised-Learning/supervised.R
+++ Session-10-Supervised-Learning/supervised.R
@@ -1,6 +1,7 @@
 library(stats)
 library(dplyr)
 
+# Plot some noisy prediction like data
 n <- 1000
 t <- rlogis(n, location=qlogis(0.1))
 noise <- rnorm(n,sd=1)
@@ -17,7 +18,7 @@ abline(h=0.5)
 
 dev.off()
 
-
+# Plot prediction and recall at different thresholds
 library(ggplot2)
 df <- data.frame(t=numeric(), p=numeric(), r=numeric())
 for (thresh in seq(0,1,0.01)) {
@@ -33,11 +34,11 @@ ggplot(df, aes(r, p, colour=t)) +
 ggsave("plots/precision-recall.png")
 
 
-
+# Load manifestos and set a variable for whether they contain the code 501
 library(readr)
 library(tidyr)
 library(dplyr)
-df <- read_csv("../datasets/uk_manifestos.csv")
+df <- read_csv("datasets/uk_manifestos.csv")
 df<- df[sample(nrow(df)),]
 df$env <- 0
 df$env[df$cmp_code==501] <- 1
@@ -51,6 +52,8 @@ library(tidymodels)
 library(parsnip)
 library(rsample)
 library(yardstick)
+
+# Train a model to predict environment
 
 df_split <- initial_split(df, prop=0.8)
 train_data <- training(df_split)
@@ -72,7 +75,12 @@ wf <- workflow() %>%
 model_fit <- wf %>% 
   fit(train_data)
 
-test_data$prediction <- predict(model_fit, test_data)$.pred_1
+p <- factor(predict(model_fit, test_data))
+head(p)
+
+# See how well the model works on our test dataset
+
+test_data$prediction <- factor(predict(model_fit, test_data)$.pred_class, levels=c(0,1))
 
 tp <- sum((test_data$env==1)&(test_data$prediction==1))
 fn <- sum((test_data$env==1)&(test_data$prediction==0))
@@ -92,6 +100,7 @@ scorer <- metric_set(
 scorer(test_data, truth=env, estimate=prediction, event_level="second")
 
 
+# Try a different model where we tune the cost function
 model <- svm_poly(cost=tune()) %>%
   set_engine("kernlab") %>%
   set_mode("classification")
@@ -100,6 +109,7 @@ wf <- workflow() %>%
   add_recipe(rec) %>%
   add_model(model)
 
+# To cross fold validation
 folds <- vfold_cv(train_data, v = 2)
 svm_res <- tune_grid(
   wf, resamples = folds, grid = 2,
@@ -113,31 +123,13 @@ best_model <- svm_res %>% select_best()
 final_workflow <- wf %>% 
   finalize_workflow(best_model)
 
-
-
+# Try the best model on our test data
 test_data$opt_prediction <- predict(model_fit, test_data)$.pred_class
-scorer(test_data, truth=env, estimate=opt_prediction)
+scorer(test_data, truth=env, estimate=opt_prediction, event_level="second")
 
 
+# Do cross fold validation to tune the threshold value
 n_splits <- 3
-
-
-
-k <- 1
-k_split <- folds$splits[[k]]
-k_train <- training(k_split)
-k_test <- testing(k_split)
-k_model <- final_workflow %>% fit(k_train)
-k_test$pred <- predict(k_model, k_test, type="prob")$.pred_1
-t <- 0.5
-est <- factor(ifelse(k_test$pred>=t,1,0))
-
-
-
-f1 <- f_meas_vec(k_test$env, est, event_level = "second")
-
-
-
 res <- data.frame(k=numeric(),t=numeric(),f1=numeric())
 n_splits=3
 folds <- vfold_cv(train_data, v = n_splits)
@@ -154,6 +146,8 @@ for (k in 1:n_splits) {
   }
 }
 
+
+# Find the threshold value that has the best f1 score
 res <- res %>% 
   group_by(t) %>% summarise(f1 = mean(f1)) %>%
   arrange(desc(f1))
